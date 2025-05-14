801797fa2b9cd023c9f7d9d536e6055828608c94
Alejandro Toro-Ossaba <alejotoro.o@gmail.com>
2025-05-14 12:11:48 -0500

    First commit (Chapter 10)

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,20 +9,24 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!); // Using docker postgres
 
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    //
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
-
+    //
+    console.log('Data fetch completed after 3 seconds.');
+    //
+    
     return data;
   } catch (error) {
     console.error('Database Error:', error);
@@ -62,6 +66,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
+    // Parallel data fetching. All fetches start executing at the same time.
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Partial Prerendering (PPR) requires nextjs canary and it's an experimental feature
+  // npm install next@canary
+  // experimental: {
+  //   ppr: 'incremental'
+  // }
 };
 
 export default nextConfig;
